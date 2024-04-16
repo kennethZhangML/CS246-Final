71631c8dc6e988519b622f40d072ae3dc5b249b7
kennethZhangML <k27zhang@mc-2063-23.student.cs.uwaterloo.ca>
2024-04-16 17:18:47 -0400

    add observer first example

---
+++ DesignPatterns/Observer/StockTrading.cc
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+class Observer {
+public:
+    virtual ~Observer() {}
+    virtual void notify() = 0;
+};
+
+class Subject { 
+    std::vector<Observer*> observers;
+public:
+    void attach(Observer *o) {
+        observers.push_back(o);
+    }
+
+    void detach(Observer* o) {
+        for (auto it = observers.begin(); it != observers.end(); ++it) {
+            if (*it == o) {
+                observers.erase(it);
+                break;
+            }
+        }
+    }
+
+    void notifyObservers() {
+        for (auto o : observers) {
+            o->notify();
+        }
+    }
+};
+
+class StockPrice : public Subject {
+    std::ifstream in;
+    int closingPrice;
+
+public:
+    StockPrice(std::string source) : in{source} {}
+
+    bool runBacktest() {
+        if (in >> closingPrice) {
+            std::cout << "Closing Price: " << closingPrice << std::endl;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    int closing() {
+        return closingPrice;
+    }
+};
+
+class Trader : public Observer {
+    StockPrice *st;
+    std::string name;
+    int optimalPrice;
+
+public:
+    Trader(StockPrice *st, std::string name, int optimalPrice) : st{st}, name{name}, optimalPrice{optimalPrice} {
+        st->attach(this);
+    }
+
+    ~Trader() {
+        st->detach(this);
+    }
+
+    void notify() override {
+        std::cout << name << ": " << (st->closing() >= optimalPrice ? "Profit!" : "Loss!") << std::endl;
+    }
+};
+
+int main() {
+    std::string fname = "stockMarket.txt";
+    StockPrice st(fname);
+    Trader trader1(&st, "Kenneth", 150000);
+    Trader trader2(&st, "Ryan", 200000);
+    Trader trader3(&st, "Jeck", 1000000);
+
+    while (st.runBacktest()) {
+        st.notifyObservers();
+    }
+
+    return 0;
+}
